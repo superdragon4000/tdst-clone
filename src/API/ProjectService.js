67a29aea2fca9f69e6fbbcf570fb51129cf420e8
Ilgizar Ismakaev <ilgizar3332@gmail.com>
2024-08-14 21:51:30 +0300

    feat: proj and task get calls

---
+++ src/API/ProjectService.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+export default class ProjectService {
+  static async getAll() {
+    const response = await axios.get("http://localhost:5000/project/all").catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
+    return response;
+  }
+
+  // static async getById(id) {
+  //   const response = await axios.get(
+  //     `https://jsonplaceholder.typicode.com/posts/${id}`
+  //   );
+  //   return response;
+  // }
+
+  // static async getCommentsByPostId(id) {
+  //   const response = await axios.get(
+  //     `https://jsonplaceholder.typicode.com/posts/${id}/comments`
+  //   );
+  //   return response;
+  // }
+}
--- src/App.js
+++ src/App.js
@@ -1,14 +1,65 @@
-import { useState } from "react";
-import TaskList from "./components/TaskList";
-import TaskForm from "./components/TaskForm";
+import { useEffect, useState } from "react";
+import ProjectService from "./API/ProjectService";
+import TaskService from "./API/TaskService"
 import "./styles/App.css";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [project, setProject] = useState(null);
+  const [isTasksLoading, setIsTasksLoading] = useState(false);
+  const [tasks, setTasks] = useState([]);
 
+
+  async function fetchProjects() {
+    setIsLoading(true);
+    const response = await ProjectService.getAll();
+    setProjects(response.data);
+    
+    setIsLoading(false);
+  }
+
+  async function fetchTasks(id) {
+    setIsTasksLoading(true);
+    const response = await TaskService.getAllById(id);
+    setTasks(response.data);
+    
+    setIsTasksLoading(false);
+  }
+
+   async function setActiveProject(e) {
+    projects.forEach(el => {
+      if(el.name === e.target.innerText) {
+        setProject(el)
+      }
+    });
+    
+    document.querySelectorAll('.project__link').forEach( btn =>
+      btn.classList.remove('active'))
+    e.currentTarget.classList.add('active');
+  }
+
+  useEffect(() => {
+    fetchProjects();
+    if(project) {
+      fetchTasks(project.id)
+    }
+  }, [project]);
 
   return (
     <div className="App">
-
+      <div className="projects">
+        {isLoading ? (
+          <p>loading</p>
+        ) : (
+          projects.map((proj) => <p onClick={e=>setActiveProject(e)} key={proj.id} className="project__link">{proj.name}</p>)
+        )}
+      </div>
+      <div className="tasks">
+        {
+          isTasksLoading
+        }
+      </div>
     </div>
   );
 }
