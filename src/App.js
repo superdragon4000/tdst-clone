906db5925ca6284bbc22d9f53a5caddb5a012c64
Ilgizar Ismakaev <ilgizar3332@gmail.com>
2024-08-15 21:32:41 +0300

    feat: render tasks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import ProjectService from "./API/ProjectService";
-import TaskService from "./API/TaskService"
+import TaskService from "./API/TaskService";
 import "./styles/App.css";
 
 function App() {
@@ -10,12 +10,11 @@ function App() {
   const [isTasksLoading, setIsTasksLoading] = useState(false);
   const [tasks, setTasks] = useState([]);
 
-
   async function fetchProjects() {
     setIsLoading(true);
     const response = await ProjectService.getAll();
     setProjects(response.data);
-    
+
     setIsLoading(false);
   }
 
@@ -23,28 +22,41 @@ function App() {
     setIsTasksLoading(true);
     const response = await TaskService.getAllById(id);
     setTasks(response.data);
-    
+
+
     setIsTasksLoading(false);
   }
 
-   async function setActiveProject(e) {
-    projects.forEach(el => {
-      if(el.name === e.target.innerText) {
-        setProject(el)
+  async function setActiveProject(e) {
+    projects.forEach((el) => {
+      if (el.name === e.target.innerText) {
+        setProject(el);
       }
     });
-    
-    document.querySelectorAll('.project__link').forEach( btn =>
-      btn.classList.remove('active'))
-    e.currentTarget.classList.add('active');
+
+    document
+      .querySelectorAll(".project__link")
+      .forEach((btn) => btn.classList.remove("active"));
+    e.currentTarget.classList.add("active");
   }
 
   useEffect(() => {
     fetchProjects();
-    if(project) {
-      fetchTasks(project.id)
+    if (project) {
+      fetchTasks(project.id);
     }
   }, [project]);
+
+
+  const renderTask = () => {
+    if (project) {
+      if (isTasksLoading) return <p>loader</p>
+      else if (tasks.length) return tasks.map((task)=><p>{task.body}</p>)
+        else return <p>no tasks</p>
+    } else {
+      return <p>select a project</p>
+    }
+  };
 
   return (
     <div className="App">
@@ -52,14 +64,20 @@ function App() {
         {isLoading ? (
           <p>loading</p>
         ) : (
-          projects.map((proj) => <p onClick={e=>setActiveProject(e)} key={proj.id} className="project__link">{proj.name}</p>)
+          projects.map((proj) => (
+            <p
+              onClick={(e) => setActiveProject(e)}
+              key={proj.id}
+              className="project__link"
+            >
+              {proj.name}
+            </p>
+          ))
         )}
       </div>
-      <div className="tasks">
-        {
-          isTasksLoading
-        }
-      </div>
+      <div className="tasks">{
+        renderTask()
+      }</div>
     </div>
   );
 }
